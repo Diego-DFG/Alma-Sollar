ec5042028f38804090c7b6158eb9bbc6fd067ff7
Diego Gomes <diegofgomes19@gmail.com>
2021-02-02 20:00:55 -0300

    Refatoracao do codigo da secao conteudo e ajustes nos elementos da secao conteudo cta

--- js/custom.js
+++ js/custom.js
@@ -12,31 +12,6 @@
 	});
 	
 	/* ..............................................
-    Fixed Menu
-    ................................................. */
-    
-	$(window).on('scroll', function () {
-		if ($(window).scrollTop() > 50) {
-			$('.top-header').addClass('fixed-menu');
-		} else {
-			$('.top-header').removeClass('fixed-menu');
-		}
-	});
-	
-	/* ..............................................
-    Gallery
-    ................................................. */
-	
-	$('#slides').superslides({
-		inherit_width_from: '.cover-slides',
-		inherit_height_from: '.cover-slides',
-		play: 5000,
-		animation: 'fade',
-	});
-	
-	$( ".cover-slides ul li" ).append( "<div class='overlay-background'></div>" );
-	
-	/* ..............................................
     Map Full
     ................................................. */
 	
