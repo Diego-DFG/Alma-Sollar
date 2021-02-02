f1e8428263afa411ffa06a61cf833135ef823f21
Diego Gomes <diegofgomes19@gmail.com>
2021-02-01 22:51:25 -0300

    Ajuste no posicionamento dos elementos da secao conteudp cta

--- js/custom.js
+++ js/custom.js
@@ -54,46 +54,4 @@
 		}); 
 	});
 	
-	/* ..............................................
-    Special Menu
-    ................................................. */
-	
-	var Container = $('.container');
-	Container.imagesLoaded(function () {
-		var portfolio = $('.special-menu');
-		portfolio.on('click', 'button', function () {
-			$(this).addClass('active').siblings().removeClass('active');
-			var filterValue = $(this).attr('data-filter');
-			$grid.isotope({
-				filter: filterValue
-			});
-		});
-		var $grid = $('.special-list').isotope({
-			itemSelector: '.special-grid'
-		});
-	});
-	
-	/* ..............................................
-    BaguetteBox
-    ................................................. */
-	
-	baguetteBox.run('.tz-gallery', {
-		animation: 'fadeIn',
-		noScrollbars: true
-	});
-	
-	
-	
-	/* ..............................................
-    Datepicker
-    ................................................. */
-	
-	$('.datepicker').pickadate();
-	
-	$('.time').pickatime();
-	
-	
-	
-	
-	
 }(jQuery));
--- js/index.js
+++
@@ -1,5 +0,0 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0];
-const navItemsList = document.getElementsByClassName('navegacao-itens')[0];
-toggleButton.addEventListener('click', () => {
-		navItemsList.classList.toggle('active');
-});
