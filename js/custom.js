0361d45cad96d8a35f92f6e49a9244d118854fc1
Diego Gomes <diegofgomes19@gmail.com>
2021-02-07 11:27:37 -0300

    Alteracao nas funcoes no botao whatsapp e melhorias nos links de navegacao

--- js/custom.js
+++ js/custom.js
@@ -23,10 +23,6 @@
 				$('#back-to-top').fadeOut(); 
 			} 
 		}); 
-		$('#back-to-top').click(function(){ 
-			$("html, body").animate({ scrollTop: 0 }, 600); 
-			return false; 
-		}); 
 	});
 	
 }(jQuery));
